0c2ea95dc5c9415ab02ee84c58add5603c3dc38c
JackSmellyDog <shaposhnikandriy@gmail.com>
2021-05-26 21:16:23 +0300

    HlrToTelegramResponseConverter refactoring

--- src/main/java/me/shaposhnik/hlrbot/converter/HlrToTelegramResponseConverter.java
+++ src/main/java/me/shaposhnik/hlrbot/converter/HlrToTelegramResponseConverter.java
@@ -5,49 +5,63 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 import static org.apache.commons.lang3.StringUtils.capitalize;
 
 
 @Component
 public class HlrToTelegramResponseConverter implements Converter<Hlr, String> {
 
+    private static final String PROVIDER_ID = "ProviderId";
+    private static final String NUMBER = "Number";
+    private static final String NETWORK = "Network";
+    private static final String STATUS = "Status";
+    private static final String PORTED = "Ported";
+    private static final String ROAMING = "Roaming";
+    private static final String CREATED_AT = "Created at";
+    private static final String STATUS_RECEIVED_AT = "Status received at";
+    private static final String DETAILS = "Details";
+    private static final String OTHER_PROPERTIES = "Other properties";
+    private static final String COLON_SPACE = ": ";
+    private static final String TAB = "    ";
+    private static final char EOL = '\n';
+
     @Override
     public String convert(@NonNull Hlr hlr) {
-        StringBuilder mainBuilder = new StringBuilder()
-            .append("***ProviderId:*** ").append(hlr.getProviderId()).append('\n')
-            .append("***Number:*** ").append(hlr.getNumber()).append('\n')
-            .append("***Network:*** ").append(hlr.getNetwork()).append('\n')
-            .append("***Status:*** ").append(capitalize(hlr.getStatus())).append('\n')
-            .append("***Ported:*** ").append(hlr.getPorted()).append('\n')
-            .append("***Roaming:*** ").append(hlr.getRoaming()).append('\n')
-            .append("***Created at:*** ").append(hlr.getCreatedAt()).append('\n')
-            .append("***Status received at:*** ").append(hlr.getStatusReceivedAt()).append('\n');
+        var mainBuilder = new StringBuilder()
+            .append(bold(PROVIDER_ID)).append(COLON_SPACE).append(hlr.getProviderId()).append(EOL)
+            .append(bold(NUMBER)).append(COLON_SPACE).append(hlr.getNumber()).append(EOL)
+            .append(bold(NETWORK)).append(COLON_SPACE).append(hlr.getNetwork()).append(EOL)
+            .append(bold(STATUS)).append(COLON_SPACE).append(capitalize(hlr.getStatus())).append(EOL)
+            .append(bold(PORTED)).append(COLON_SPACE).append(hlr.getPorted()).append(EOL)
+            .append(bold(ROAMING)).append(COLON_SPACE).append(hlr.getRoaming()).append(EOL)
+            .append(bold(CREATED_AT)).append(COLON_SPACE).append(hlr.getCreatedAt()).append(EOL)
+            .append(bold(STATUS_RECEIVED_AT)).append(COLON_SPACE).append(hlr.getStatusReceivedAt()).append(EOL);
 
-        final Map<String, String> details = hlr.getDetails();
+        final var details = hlr.getDetails();
         if (details != null && !details.isEmpty()) {
             StringBuilder detailsBuilder = new StringBuilder();
 
-            details.forEach((k, v) ->
-                detailsBuilder.append("    ***").append(k).append("***").append(": ").append(v).append('\n'));
+            details.forEach((k, v) -> 
+                detailsBuilder.append(TAB).append(bold(k)).append(COLON_SPACE).append(v).append(EOL));
 
-            mainBuilder.append("***Details:*** ").append('\n')
-                .append(detailsBuilder);
+            mainBuilder.append(bold(DETAILS)).append(COLON_SPACE).append(EOL).append(detailsBuilder);
         }
 
-        final Map<String, String> otherProperties = hlr.getOtherProperties();
+        final var otherProperties = hlr.getOtherProperties();
 
         if (otherProperties != null && !otherProperties.isEmpty()) {
-            StringBuilder otherPropertiesBuilder = new StringBuilder();
+            var otherPropertiesBuilder = new StringBuilder();
 
             otherProperties.forEach((k, v) ->
-                otherPropertiesBuilder.append("    ***").append(k).append("***").append(": ").append(v).append('\n'));
+                otherPropertiesBuilder.append(TAB).append(bold(k)).append(COLON_SPACE).append(v).append(EOL));
 
-            mainBuilder.append("***Other properties:*** ").append('\n')
-                .append(otherPropertiesBuilder);
+            mainBuilder.append(bold(OTHER_PROPERTIES)).append(COLON_SPACE).append(EOL).append(otherPropertiesBuilder);
         }
 
         return mainBuilder.toString();
+    }
+    
+    private String bold(String text) {
+        return String.format("***%s***", text);
     }
 }
